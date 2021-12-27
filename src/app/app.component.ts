1f5cae28724677cfecfad827438682d606b56a56
vikaas-1 <pal.vikaas@gmail.com>
2021-12-27 18:22:30 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  public searchText: string;
+  public employee: string;
+  public employeeList: any;
+  items: Array<any> = [];
+  newItem: any = {};
+  ngOnInit() {
+    this.employeeList = [
+      {
+        id: 1,
+        first_name: 'Jeanette',
+        last_name: 'Penddreth',
+        email: 'jpenddreth0@census.gov',
+        gender: 'Female',
+      },
+      {
+        id: 2,
+        first_name: 'Giavani',
+        last_name: 'Frediani',
+        email: 'gfrediani1@senate.gov',
+        gender: 'Male',
+      },
+      {
+        id: 3,
+        first_name: 'Noell',
+        last_name: 'Bea',
+        email: 'nbea2@imageshack.us',
+        gender: 'Female',
+      },
+      {
+        id: 4,
+        first_name: 'Willard',
+        last_name: 'Valek',
+        email: 'wvalek3@vk.com',
+        gender: 'Male',
+      },
+    ];
+  }
+
+  addEmployee() {
+    this.items.push(this.newItem);
+    this.newItem = {};
+  }
 }
